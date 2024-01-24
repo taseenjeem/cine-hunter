1bfe18089a14c906e3605b90a02d5065a9cd3620
taseenjeem <mdtaseenfuad@gmail.com>
2024-01-24 15:31:30 +0600

    added cart details modal

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,19 @@ import bellIcon from "./assets/ring.svg";
 import moonIcon from "./assets/icons/moon.svg";
 import logo from "./assets/logo.svg";
 import cartIcon from "./assets/shopping-cart.svg";
+import { useState } from "react";
+import CartDetails from "./cine/CartDetails";
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleShowCart = () => {
+    setShowCart(true);
+  };
+
   return (
     <>
+      {showCart && <CartDetails onCloseCartModal={() => setShowCart(false)} />}
       <header>
         <nav className="container flex items-center justify-between space-x-10 py-6">
           <a href="index.html">
@@ -32,6 +41,7 @@ const Header = () => {
               <a
                 className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                 href="#"
+                onClick={handleShowCart}
               >
                 <img src={cartIcon} width={24} height={24} alt="" />
               </a>
