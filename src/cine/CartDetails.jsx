280694f69cb2a3f9e6b822a00740ae10b93797ec
taseenjeem <mdtaseenfuad@gmail.com>
2024-01-24 23:27:30 +0600

    remove movie from cart

--- src/cine/CartDetails.jsx
+++ src/cine/CartDetails.jsx
@@ -1,4 +1,20 @@
+import { useContext } from "react";
+import { MovieContext } from "../context";
+import deleteIcon from "../assets/delete.svg";
+import checkoutIcon from "../assets/icons/checkout.svg";
+import { getImageURL } from "../utils/cine-utility";
+
 const CartDetails = ({ onCloseCartModal }) => {
+  const { cartData, setCartData } = useContext(MovieContext);
+
+  const handleRemoveMovie = (event, movieID) => {
+    event.preventDefault();
+
+    const remainingMovies = cartData.filter((item) => item.id !== movieID);
+
+    setCartData([...remainingMovies]);
+  };
+
   return (
     <>
       <section className="dark:bg-body bg-white font-[Sora] dark:text-white text-dark">
@@ -10,74 +26,51 @@ const CartDetails = ({ onCloseCartModal }) => {
                 Your Carts
               </h2>
               <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
-                <div className="grid grid-cols-[1fr_auto] gap-4">
-                  <div className="flex items-center gap-4">
-                    <img
-                      className="rounded overflow-hidden"
-                      src="/assets/cart-item.png"
-                      alt=""
-                    />
-                    <div>
-                      <h3 className="text-base md:text-xl font-bold">
-                        Iron Man
-                      </h3>
-                      <p className="max-md:text-xs text-[#575A6E]">
-                        Action/Adventure/Sci-fi
-                      </p>
-                      <span className="max-md:text-xs">$100</span>
+                {cartData.length === 0 ? (
+                  <p className="text-center text-lg font-semibold text-red-500">
+                    No Movies Found
+                  </p>
+                ) : (
+                  cartData.map((data) => (
+                    <div
+                      key={data.id}
+                      className="grid grid-cols-[1fr_auto] gap-4"
+                    >
+                      <div className="flex items-center gap-4">
+                        <img
+                          className="rounded max-w-16 w-full overflow-hidden"
+                          src={getImageURL(data.cover)}
+                          alt=""
+                        />
+                        <div>
+                          <h3 className="text-base md:text-xl font-bold">
+                            {data.title}
+                          </h3>
+                          <p className="max-md:text-xs text-[#575A6E]">
+                            {data.genre}
+                          </p>
+                          <span className="max-md:text-xs">${data.price}</span>
+                        </div>
+                      </div>
+                      <div className="flex justify-between gap-4 items-center">
+                        <button
+                          onClick={(event) => handleRemoveMovie(event, data.id)}
+                          className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
+                        >
+                          <img className="w-5 h-5" src={deleteIcon} alt="" />
+                          <span className="max-md:hidden">Remove</span>
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex justify-between gap-4 items-center">
-                    <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
-                      <img
-                        className="w-5 h-5"
-                        src="./assets/delete.svg"
-                        alt=""
-                      />
-                      <span className="max-md:hidden">Remove</span>
-                    </button>
-                  </div>
-                </div>
-                <div className="grid grid-cols-[1fr_auto] gap-4">
-                  <div className="flex items-center gap-4">
-                    <img
-                      className="rounded overflow-hidden"
-                      src="/assets/cart-item.png"
-                      alt=""
-                    />
-                    <div>
-                      <h3 className="text-base md:text-xl font-bold">
-                        Iron Man
-                      </h3>
-                      <p className="max-md:text-xs text-[#575A6E]">
-                        Action/Adventure/Sci-fi
-                      </p>
-                      <span className="max-md:text-xs">$100</span>
-                    </div>
-                  </div>
-                  <div className="flex justify-between gap-4 items-center">
-                    <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
-                      <img
-                        className="w-5 h-5"
-                        src="./assets/delete.svg"
-                        alt=""
-                      />
-                      <span className="max-md:hidden">Remove</span>
-                    </button>
-                  </div>
-                </div>
+                  ))
+                )}
               </div>
               <div className="flex items-center justify-end gap-2">
                 <a
                   className="rounded-md p-2 md:px-4 inline-flex items-center space-x-2 bg-primary text-[#171923] text-sm"
                   href="#"
                 >
-                  <img
-                    src="./assets/icons/checkout.svg"
-                    width={24}
-                    height={24}
-                    alt=""
-                  />
+                  <img src={checkoutIcon} width={24} height={24} alt="" />
                   <span>Checkout</span>
                 </a>
                 <a
